422cf6f686d7ec6078712314ce222e77358af8ab
James <trentphoto@gmail.com>
2025-08-26 16:09:43 -0400

    fix rolebanner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -44,7 +44,7 @@ export default async function Home() {
   return (
     <main className="min-h-screen flex flex-col items-center">
       <div className="flex-1 flex flex-col gap-20 p-8">
-        <RoleBanner />
+        <RoleBanner role={role} />
         {renderDashboard()}
       </div>
     </main>
